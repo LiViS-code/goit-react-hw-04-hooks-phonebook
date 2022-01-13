d4c8db33238493b26388f5dbd86d506bd969f273
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-13 13:45:05 +0200

    refactoring via hooks

--- my-phonebook/src/App.js
+++ my-phonebook/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import "react-toastify/dist/ReactToastify.css";
 import { nanoid } from "nanoid";
 import ContactForm from "./components/ContactForm/ContactForm";
@@ -8,102 +8,90 @@ import { Container, Logo, Title, ContactsTitle, Message } from "./App.styled";
 import toastMsg from "./utils/toastMsg";
 import phonebook from "./img/phonebook.png";
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: "",
-  };
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState("");
 
-  onChangeState = (name, number) => {
-    if (this.matchCheckName(name, this.state.contacts)) {
+  const onChangeState = (name, number) => {
+    if (matchCheckName(name, contacts)) {
       toastMsg(name, "warn");
       return "not success";
     }
 
     const newContact = [{ id: nanoid(), name: name, number: number }];
 
-    this.setState((s) => ({
-      contacts: s.contacts.concat(newContact),
-    }));
+    setContacts([...contacts, ...newContact]);
+
     toastMsg(name, "success");
     return "success";
   };
 
-  matchCheckName = (name, contacts) => {
+  const matchCheckName = (name, contacts) => {
     for (let i = 0; i < contacts.length; i += 1) {
       if (contacts[i].name === name) return true;
     }
     return false;
   };
 
-  onFilter = (word) => {
-    this.setState({ filter: word.toLowerCase() });
+  const onFilter = (word) => {
+    setFilter(word ? word.toLowerCase() : "");
   };
 
-  onDelete = (id) => {
-    const newContacts = this.state.contacts;
+  const onDelete = (id) => {
+    const newContacts = contacts;
 
     for (let i = 0; i < newContacts.length; i += 1) {
       if (newContacts[i].id === id) {
         const name = newContacts[i].name;
         newContacts.splice(i, 1);
-        this.setState({ contacts: newContacts });
+        setContacts(newContacts);
         toastMsg(name, "info");
         break;
       }
     }
 
-    if (this.state.contacts.length <= 1) {
-      this.onFilter("");
+    if (contacts.length <= 1) {
+      onFilter();
     }
   };
 
-  componentDidMount = () => {
+  useEffect(() => {
     if (localStorage.getItem("contacts")) {
-      this.setState({
-        contacts: JSON.parse(localStorage.getItem("contacts")),
-      });
+      setContacts(JSON.parse(localStorage.getItem("contacts")));
     }
-  };
+  }, []);
 
-  componentDidUpdate = (prevState) => {
-    if (prevState.contacts !== this.state.contacts)
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-  };
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
-  render() {
-    const {
-      state: { contacts, filter },
-      onChangeState,
-      onFilter,
-      onDelete,
-    } = this;
-    const onContctsGroup = contacts.length !== 0 ? true : false;
-    const onContactsFilter = contacts.length >= 2 ? true : false;
+  const onContactsGroup = contacts.length !== 0 ? true : false;
+  const onContactsFilter = contacts.length >= 2 ? true : false;
+  console.log("contacts.length", contacts.length);
+  console.log("contacts", contacts);
+  console.log("onContactsGroup", onContactsGroup);
+  console.log("onContactsFilter", onContactsFilter);
 
-    return (
-      <Container>
-        <Title>
-          <Logo src={phonebook} alt="fonebook" width="50px" />
-          Phonebook
-        </Title>
-        <ContactForm contacts={contacts} onChangeState={onChangeState} />
-        {onContctsGroup ? (
-          <>
-            <ContactsTitle>Contacts</ContactsTitle>
-            {onContactsFilter && <Filter onFilter={onFilter} filter={filter} />}
-            <ContactList
-              contacts={contacts}
-              filter={filter}
-              onDelete={onDelete}
-            />
-          </>
-        ) : (
-          <Message>You have no saved contacts</Message>
-        )}
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Title>
+        <Logo src={phonebook} alt="fonebook" width="50px" />
+        Phonebook
+      </Title>
+      <ContactForm contacts={contacts} onChangeState={onChangeState} />
+      {onContactsGroup ? (
+        <>
+          <ContactsTitle>Contacts</ContactsTitle>
+          {onContactsFilter && <Filter onFilter={onFilter} filter={filter} />}
+          <ContactList
+            contacts={contacts}
+            filter={filter}
+            onDelete={onDelete}
+          />
+        </>
+      ) : (
+        <Message>You have no saved contacts</Message>
+      )}
+    </Container>
+  );
 }
-
-export default App;
