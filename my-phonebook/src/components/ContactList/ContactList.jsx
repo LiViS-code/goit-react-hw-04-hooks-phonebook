12aa395c3ec689947d9536173a2d9ad2f330569c
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-13 14:44:09 +0200

    refactoring via hooks

--- my-phonebook/src/components/ContactList/ContactList.jsx
+++ my-phonebook/src/components/ContactList/ContactList.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { IconContext } from "react-icons";
 import { FcDeleteDatabase } from "react-icons/fc";
@@ -9,46 +8,44 @@ import {
   Link,
 } from "./ContactList.styled";
 import { Button } from "../ContactForm/ContactForm.styled";
-class ContactList extends Component {
-  render() {
-    const { contacts, filter, onDelete } = this.props;
-    let filterContacts = contacts;
 
-    if (filter) {
-      filterContacts = contacts.filter(({ name }) =>
-        name.toLowerCase().includes(filter)
-      );
-    }
+export default function ContactList({ contacts, filter, onDelete }) {
+  let filterContacts = contacts;
 
-    return (
-      <ContastsList>
-        {filterContacts.map(({ id, name, number }) => (
-          <ContactItem key={id}>
-            <ContactNote>
-              {name}:{" "}
-              <Link href={"tel:+" + parseInt(number.replace(/\D+/g, ""))}>
-                {number}
-              </Link>
-            </ContactNote>
-            <Button
-              type="button"
-              onClick={() => onDelete(id)}
-              title="Delete this contact"
-            >
-              <IconContext.Provider
-                value={{
-                  size: "1.3em",
-                  style: { verticalAlign: "middle" },
-                }}
-              >
-                <FcDeleteDatabase />
-              </IconContext.Provider>
-            </Button>
-          </ContactItem>
-        ))}
-      </ContastsList>
+  if (filter) {
+    filterContacts = contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter)
     );
   }
+
+  return (
+    <ContastsList>
+      {filterContacts.map(({ id, name, number }) => (
+        <ContactItem key={id}>
+          <ContactNote>
+            {name}:{" "}
+            <Link href={"tel:+" + parseInt(number.replace(/\D+/g, ""))}>
+              {number}
+            </Link>
+          </ContactNote>
+          <Button
+            type="button"
+            onClick={() => onDelete(id)}
+            title="Delete this contact"
+          >
+            <IconContext.Provider
+              value={{
+                size: "1.3em",
+                style: { verticalAlign: "middle" },
+              }}
+            >
+              <FcDeleteDatabase />
+            </IconContext.Provider>
+          </Button>
+        </ContactItem>
+      ))}
+    </ContastsList>
+  );
 }
 
 ContactList.propTypes = {
@@ -56,5 +53,3 @@ ContactList.propTypes = {
   filter: PropTypes.string.isRequired,
   onDelete: PropTypes.func.isRequired,
 };
-
-export default ContactList;
