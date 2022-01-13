9def49e6e3b76e97fd0b02388e30303190f01037
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-13 14:29:51 +0200

    refactoring via hooks

--- my-phonebook/src/components/Filter/Filter.jsx
+++ my-phonebook/src/components/Filter/Filter.jsx
@@ -1,40 +1,31 @@
-import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { FcFilledFilter } from "react-icons/fc";
 import { Container } from "./Filter.styled";
 import { Input, Label } from "../ContactForm/ContactForm.styled";
 
-class Filter extends Component {
-  handleInput = (e) => {
-    const { value } = e.target;
-    const { onFilter } = this.props;
-    onFilter(value);
+export default function Filter({ onFilter }, { filter }) {
+  const handleInput = (e) => {
+    onFilter(e.target.value);
   };
 
-  render() {
-    const { handleInput } = this;
-    const { filter } = this.props;
-    return (
-      <Container>
-        <Label htmlFor="inputFilter">
-          <FcFilledFilter />
-          Find contacts by name
-        </Label>
-        <Input
-          id="inputFilter"
-          type="text"
-          value={filter}
-          name="filter"
-          onChange={handleInput}
-        ></Input>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Label htmlFor="inputFilter">
+        <FcFilledFilter />
+        Find contacts by name
+      </Label>
+      <Input
+        id="inputFilter"
+        type="text"
+        value={filter}
+        name="filter"
+        onChange={handleInput}
+      ></Input>
+    </Container>
+  );
 }
 
 Filter.propTypes = {
   onFilter: PropTypes.func.isRequired,
   filter: PropTypes.string.isRequired,
 };
-
-export default Filter;
